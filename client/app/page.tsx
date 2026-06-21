6957abfce8f498bacc14ba24dcc83bea167efb2e
Glen Barasa <glenbarasa4@gmail.com>
2026-06-21 23:22:04 +0300

    feat: impliment resposive navbar

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import AnoAI from "@/components/ui/animated-shader-background";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-black">
+      <AnoAI />
+    </div>
+  );
+}
